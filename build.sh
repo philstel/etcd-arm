f6448fee9e8705764a5579eef44158f07f49f3e8
kylejw1 <kylejw1@gmail.com>
2016-12-01 22:39:00 -0700

    Clean up image after build

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@
 docker build -f Dockerfile.build -t kylejw/etcd-arm-build .
 docker run --rm -v $(pwd):/out kylejw/etcd-arm-build
 docker build -f Dockerfile.etcd -t kylejw/etcd-arm .
-#docker rmi kylejw/etcd-arm-build
+docker rmi kylejw/etcd-arm-build
