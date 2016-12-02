362f4e4b4718957be1b55d9a3413aeea9ee7a053
kylejw1 <kylejw1@gmail.com>
2016-12-01 22:38:05 -0700

    Fixed a few issues, set etcd branch to 3.0.15

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 
 docker build -f Dockerfile.build -t kylejw/etcd-arm-build .
 docker run --rm -v $(pwd):/out kylejw/etcd-arm-build
-docker build -f Dockerfile.etcd -t kylejw.etcd-arm
+docker build -f Dockerfile.etcd -t kylejw/etcd-arm .
 #docker rmi kylejw/etcd-arm-build
