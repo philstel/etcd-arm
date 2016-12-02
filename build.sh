8e827fde1d5edf9ab98daaeb94c93b3be28c7108
kylejw1 <kylejw1@gmail.com>
2016-12-01 22:16:46 -0700

    Initial commit.  Builds a compilation container which cross compiles etcd to armv7, then copies etcd and etcdctl to a scratch container

---
+++ build.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+docker build -f Dockerfile.build -t kylejw/etcd-arm-build .
+docker run --rm -v $(pwd):/out kylejw/etcd-arm-build
+docker build -f Dockerfile.etcd -t kylejw.etcd-arm
+#docker rmi kylejw/etcd-arm-build
